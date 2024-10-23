d80bb550b951832fad563899f6c1cd51edff2c1a
rinjugatla <rin.jugatla@gmail.com>
2024-10-23 23:53:49 +0900

    クライアント側をクラス化

--- client.py
+++ client.py
@@ -2,54 +2,62 @@
 import websocket
 import rel
 
-def on_message(ws, message):
-    """メッセージ受信時に実行
+class Client():
+    def __init__(self, host, port):
+        # ログを詳細に表示する場合はコメントアウト
+        # websocket.enableTrace(True)
+        self.client = websocket.WebSocketApp(
+            f"ws://{host}:{port}",
+            on_open=self.on_open,
+            on_message=self.on_message,
+            on_error=self.on_error,
+            on_close=self.on_close)
+        
+    def on_message(self, ws, message):
+        """メッセージ受信時に実行
 
-    Args:
-        ws (_type_): WebSocket
-        message (_type_): メッセージ
-    """
-    print(f"message: {message}")
+        Args:
+            ws (_type_): WebSocket
+            message (_type_): メッセージ
+        """
+        print(f"message: {message}")
 
-def on_error(ws, error):
-    """エラー発生時に実行
+    def on_error(self, ws, error):
+        """エラー発生時に実行
 
-    Args:
-        ws (_type_): WebSocket
-        error (_type_): エラー
-    """
-    print(f"error: {error}")
+        Args:
+            ws (_type_): WebSocket
+            error (_type_): エラー
+        """
+        print(f"error: {error}")
 
-def on_close(ws, status_code, message):
-    """通信終了時に実行
+    def on_close(self, ws, status_code, message):
+        """通信終了時に実行
 
-    Args:
-        ws (_type_): WebSocket
-        status_code (_type_): ステータス
-        message (_type_): メッセージ
-    """
-    print("closed connection")
+        Args:
+            ws (_type_): WebSocket
+            status_code (_type_): ステータス
+            message (_type_): メッセージ
+        """
+        print("closed connection")
 
-def on_open(ws):
-    """通信開始時に実行
+    def on_open(self, ws):
+        """通信開始時に実行
 
-    Args:
-        ws (_type_): WebSocket
-    """
-    print("opened connection")
+        Args:
+            ws (_type_): WebSocket
+        """
+        print("opened connection")
+
+    def run(self):
+        """クライアントを起動
+        """
+        self.client.run_forever(dispatcher=rel, reconnect=5)
+        rel.signal(2, rel.abort)
+        rel.dispatch()
 
 if __name__ == "__main__":
-    # ログを詳細に表示する場合はコメントアウト
-    # websocket.enableTrace(True)
-
-    ws = websocket.WebSocketApp("ws://127.0.0.1:12355",
-                                on_open=on_open,
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
-
-    # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
-    ws.run_forever(dispatcher=rel, reconnect=5)
-    # Keyboard Interrupt
-    rel.signal(2, rel.abort)
-    rel.dispatch()
+    HOST = "localhost"
+    PORT = 12355
+    client = Client(HOST, PORT)
+    client.run()
